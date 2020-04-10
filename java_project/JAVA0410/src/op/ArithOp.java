1db9ef82c2db8026048d7f7e447430b0385a3cbb
a4-01 <jinmyeong486@gmail.com>
2020-04-10 12:30:56 +0900

    실습

---
+++ java_project/JAVA0410/src/op/ArithOp.java
@@ -0,0 +1,37 @@
+package op;
+
+public class ArithOp {
+
+	public static void main(String[] args) {
+		// 1. 숫자 정수 두 개를 받는다( 변수선언 )
+		int num1;
+		int num2;
+
+		num1 = 10;
+		num2 = 20;
+
+		// 2. 덧셈 연산을 한다. 결과를 출력
+		int result = num1 + num2;
+
+		System.out.println("num1 + num2 = " + result);
+
+		// 3. 빼기 연산을한다. 결과를 출력
+		result = num1 - num2;
+
+		System.out.println("num1 - num2 = " + result);
+
+		// 4. 곱의 연산 -> 결과 출력
+		result = num1 * num2;
+		System.out.println("num1 * num2 = " + result);
+
+		// 5. 나누기 연산 -> 결과 출력
+		result = num1 / num2;
+		System.out.println("num1 / num2 = " + result);
+
+		// 6. 나머지연산 -> 결과 출력
+		result = num1 / num2;
+		System.out.println("num1 / num2 = " + result);
+
+	}
+
+}
