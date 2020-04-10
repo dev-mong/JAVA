57db32907948e92610f1ebf38d21f8f9fde1e363
a4-01 <jinmyeong486@gmail.com>
2020-04-10 13:05:19 +0900

    실습

---
+++ Java/JAVA0410/src/op/LogicOp.java
@@ -0,0 +1,18 @@
+package op;
+
+public class LogicOp {
+
+	public static void main(String[] args) {
+		
+		// 정수형 데이터 두 개를 받는다.
+		// int num1 = 10 ,num2 = 20;
+		int num1 = 10; // 사용자에게 받는 첫번쨰 숫자 
+		int num2 = 20; // 사용자에게 받는 두번째 숫자
+		
+		//num1이 num2 보다 큰지 확인
+		//큰 경우에는 true, 작은 경우 false
+		
+
+	}
+
+}
