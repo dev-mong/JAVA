8807d0302339b1841f6402a5ff648fe2760e592b
qkrwlsaud <63437506+qkrwlsaud@users.noreply.github.com>
2020-04-12 17:33:43 +0900

    예제 풀이

--- JAVA/Study/src/Study/Op7.java
+++ JAVA/Study/src/Study/Op7.java
@@ -4,16 +4,17 @@ public class Op7 {
 
 	public static void main(String[] args) {
 	
-//		변수num의 값보다 크면서도 가장 가까운10의 배수에서 변수num의 값을뺀 나머지를 구하는 코드이다.
-//		예를 들어, 24의 크면서도 가장 가까운10의 배수는 30이다.
-//		19의 경우 20이고,81의 경우 90이 된다.
-//		30에서 24를 뺀 나머지는6이기 때문에 변수num의 값이 24라면6을결과로 얻어야 한다.
+//		화씨(Fahrenheit)를 섭씨(Celcius)로 변환하는 코드이다.
+//		변환공식이 'C =5/9 ×(F - 32)'라고 할 때,(1)에 알맞은 코드를 넣으시오.
+//		단, 변환 결과값은 소수점셋째 자리에서 반올림해야 한다.
 		
-		int num = 24;
-		int result;
-		result = (((num / 10) + 1) * 10 ) - num;
+		int fahrenheit = 100;
 		
-		System.out.println(result);
+		float celcius = (float)(5 / 9 * (fahrenheit - 32));
+		
+		System.out.println("Fahrenheit" + fahrenheit);
+		System.out.println("Celcius" + celcius);
+		
 		
 		
 	}
---
+++ JAVA/Study/src/Study/test.java
@@ -0,0 +1,10 @@
+package Study;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("이것은 \r 앞으로 \n");
+
+	}
+
+}
