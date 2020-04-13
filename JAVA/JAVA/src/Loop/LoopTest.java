0ba533e4ebca53d4a0b355fb50c12c91164ab813
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-13 15:04:00 +0900

    반복문 실습

---
+++ JAVA/JAVA/src/Loop/LoopTest.java
@@ -0,0 +1,30 @@
+package Loop;
+
+public class LoopTest {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 51; i++) {
+			System.out.println(i);
+		}
+
+		System.out.println("--------------");
+		
+		int i = 1;
+
+		while (i < 51) {
+			System.out.println(i);
+			i++; // ☆☆☆☆☆☆☆중요☆☆☆☆☆☆
+		}
+		
+		System.out.println("--------------");
+		i = 1;
+		
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<51);
+
+	}
+
+}
