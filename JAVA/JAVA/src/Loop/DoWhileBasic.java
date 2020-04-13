e7ca741b52bf6a52329d97a0d73f251bf0b7faed
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-13 15:18:23 +0900

    do~while문 예제

---
+++ JAVA/JAVA/src/Loop/DoWhileBasic.java
@@ -0,0 +1,15 @@
+package Loop;
+
+public class DoWhileBasic {
+
+	public static void main(String[] args) {
+		int num = 0; //반복의 조건을 체크하는 변수
+		
+		do {
+			System.out.println("I like JAVA" + num);
+			num ++;
+		}while(num < 5);
+
+	}
+
+}
