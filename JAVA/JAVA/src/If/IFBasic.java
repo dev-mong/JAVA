d713f692ed4360ac73bb0a022533de73975725a0
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-13 12:02:12 +0900

    if문 예제

--- JAVA/JAVA/src/If/IFBasic.java
+++ JAVA/JAVA/src/If/IFBasic.java
@@ -2,11 +2,34 @@ package If;
 
 public class IFBasic {
 	public static void main(String[] args) {
-		
-		if(true)
-		{
+
+		if (true) {
 			System.out.println("true 일 때 출력");
 		}
+
+//		 오류
+//		if(else)
+//		{
+//			System.out.println("true 일 때 출력");
+//		}
+
+		// dead code : 문법적인 오류는 없으나 의미 없는 코드
+		if (false) {
+			System.out.println("true 일 때 출력");
+		} else {
+			System.out.println("false 일 떄 출력");
+		}
+
+		if (10 > 5) {
+			System.out.println("10은 5보다 큽니다.");
+		}
 		
+		//변수가 선언되어 dead code는 아님 
+		int num = 10;
+		
+		if (num == 5) {
+			System.out.println("10은 5와 같습니다.");
+		}
+
 	}
 }
