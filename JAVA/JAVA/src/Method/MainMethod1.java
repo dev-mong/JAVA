a285ec7deb1d4b1ff6320a6ed73bf2f3baae0466
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-14 11:19:13 +0900

    클래스 인스턴스 예제

---
+++ JAVA/JAVA/src/Method/MainMethod1.java
@@ -0,0 +1,18 @@
+package Method;
+
+public class MainMethod1 {
+
+	public static void main(String[] args) {
+		//메서드를 사용하기 위해서 인스턴스화
+		//메서드 코드를 메모리에 로드
+		//인스턴스 생성 ( 객체 생성 )
+
+		Method2 math = new Method2();
+	}
+	
+	int add() {
+		return 0;
+		
+	}
+
+}
---
+++ JAVA/JAVA/src/Method/Method2.java
@@ -0,0 +1,16 @@
+package Method;
+
+public class Method2 {
+
+	public static void main(String[] args) {
+		//더하기 기능의 메서드를 가지는 클래스
+		// 정수형 데이터 두개를 전달 받아
+		// 두 수의 합을 연산하고 그 결과 반환 메서드 생성		
+	}
+	
+	long add(long n1, long n2) {
+		long result = n1 + n2;
+		return result;
+	}
+
+}
