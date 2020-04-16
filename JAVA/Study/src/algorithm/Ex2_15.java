ce11b829d8912939cb7c515aa86fceb4355335b3
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-16 12:41:34 +0900

    회문수 프로그램

--- JAVA/Study/src/algorithm/Ex2_15.java
+++ JAVA/Study/src/algorithm/Ex2_15.java
@@ -4,23 +4,28 @@ public class Ex2_15 {
 
 	public static void main(String[] args) {
 //		회문수 프로그램
-		
+
 		int number = 12321;
 		int tmp = number;
-		int result = 0; //변수를 거꾸로 변환해서 담을 변수
-		
+		int result = 0; // 변수를 거꾸로 변환해서 담을 변수
+
 		while(tmp != 0) {
-//			for(int i=10000;i>=1;i/=10) {
-//				result = number/10000 % 10;
-//				System.out.println(result);
-//			}
-			result = number % tmp;
-		}
-		if(number == result) {
+			
+			int j = 1;
+			for (int i = 10000; i >= 1; i /= 10) {
+				tmp = number / i % 10 * j;
+				j = j*10;
+				result +=tmp;
+			}
+			System.out.println(result);
+			break;
+		}	
+		
+		if (number == result) {
 			System.out.println(number + "는 회문수 입니다.");
 		} else {
-			System.out.println(number+"는 회문수가 아닙니다.");
-		}	
+			System.out.println(number + "는 회문수가 아닙니다.");
+		}
 	}
 
 }
