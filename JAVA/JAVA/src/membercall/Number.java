8d5e8a1e5c5fdb2470693e20f0c2222666f4de38
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-16 18:02:38 +0900

    생성자

--- JAVA/JAVA/src/membercall/Number.java
+++ JAVA/JAVA/src/membercall/Number.java
@@ -3,34 +3,27 @@ package membercall;
 
 public class Number {
 	
-	//인스턴스 변수
-	int iv =10;
-	// 클래스 변수, static 변수
-	static int cv = 100;
+	int num;
 	
-	int iv2 = cv; //인스턴스 변수에 static 변수 값을 넣었기에 가능 
-//	static int cv2 = iv; // 오류 발생,
-	// 해결방법
-	static int cv2 = new Number().iv; //인스턴스 생성 후 선언, 한번만 사용 할 떄
+//	Number (){
+//		System.out.println("생성자 호출"); // number 클래스 초기화 
+//		num = 1000;
+//	}
 	
-	
-	static void staticMethod() {
-		// static 멤버 출력
-		System.out.println(cv);
+	Number(int n){ // 생성자
+		System.out.println("생성자 호출");
+		num = n;
+		System.out.println(num);
+	}
+
+	public static void main(String[] args) {
 		
-		// 인스턴스 멤버 출력
-//		System.out.println(iv);
+		// 객체를 생성 후 변수를 참조
+//		Number n1 = new Number();
+//		System.out.println(n1.num);
 		
-		Number mc = new Number();
-		System.out.println(mc.iv);
+		// 생성자에 매개변수 전달 
+		Number n1 = new Number(1000000);
 		
 	}
-	
-	void instanceMethod() {
-		// static 멤버 출력
-		System.out.println(cv);
-		// 인스턴스 멤버 출력
-		System.out.println(iv);
-	}
-	
 }
