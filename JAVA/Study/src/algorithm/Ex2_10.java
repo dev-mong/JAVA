d5f4b65c805ca210ec3de717c7ba41609bd0b156
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-16 10:04:19 +0900

    12345의 각 자리 수 합

--- JAVA/Study/src/algorithm/Ex2_10.java
+++ JAVA/Study/src/algorithm/Ex2_10.java
@@ -6,19 +6,15 @@ public class Ex2_10 {
 	public static void main(String[] args) {
 //		12345의 각 자리 수 합
 
-		int num = 12345 ;
+		int num = 12345;
 		int sum = 0;
 		
-		int value = num % 10;
-		System.out.println(value);
+		int value;
 		
-		value = num % 100;
-		System.out.println(value);
-		value = num / 100;
-		System.out.println(value);
-		value = num / 10000;
-		System.out.println(value);
-		
+		for(int i=10000;i>=1;i/=10) {
+			value = (num/i) % 10;
+			sum +=value;
+		}
 		System.out.println("Sum: "+ sum);
 		
 	}
