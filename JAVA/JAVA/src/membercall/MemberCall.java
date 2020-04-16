75579bb7831291f1f40a676b1b20f54a0c9bc9f0
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-16 17:54:00 +0900

    instance 변수, static 변수

---
+++ JAVA/JAVA/src/membercall/MemberCall.java
@@ -0,0 +1,30 @@
+package membercall;
+
+public class MemberCall {
+	
+	//인스턴스 변수
+	int iv =10;
+	// 클래스 변수, static 변수
+	static int cv = 100;
+	
+	int iv2 =cv; //인스턴스 변수에 static 변수 값을 넣었기에 가능 
+//	static int cv2 = iv; // 오류 발생,
+	// 해결방법
+	static int cv2 = new MemberCall().iv; //인스턴스 생성 후 선언, 한번만 사용 할 떄
+	
+	
+	void staticMethod() {
+		// static 멤버 출력
+		System.out.println(cv);
+		// 인스턴스 멤버 출력
+		System.out.println(iv);
+	}
+	
+	void instanceMethod() {
+		// static 멤버 출력
+		System.out.println(cv);
+		// 인스턴스 멤버 출력
+		System.out.println(iv);
+	}
+	
+}
