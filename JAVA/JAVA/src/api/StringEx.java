ef22e06550dba8c814a7f18a03768f78506fc8df
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-17 17:39:13 +0900

    문자열 예제

---
+++ JAVA/JAVA/src/api/StringEx.java
@@ -0,0 +1,25 @@
+package api;
+
+public class StringEx {
+
+	public static void main(String[] args) {
+		
+		String str = "My name is JAVA";
+		
+		int strLength = str.length();
+		
+		System.out.println("문자열 str의 문자열의 길이 : "+strLength);
+		int strLength2 = "한글의 길이는?".length();
+		System.out.println("길이 : "+strLength2);
+	
+		System.out.println("----------------------");
+		String str1 = "My String";
+		String str2 = "My String";
+		String str3 = "Your String";
+		
+		System.out.println(str1==str2); 
+		System.out.println(str==str3);
+		
+	}
+
+}
