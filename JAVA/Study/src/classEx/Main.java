03cd927efafa74f20d27d31efef2d72422e45492
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-17 10:13:25 +0900

    삼각형 넓이 클래스

--- JAVA/Study/src/classEx/Main.java
+++ JAVA/Study/src/classEx/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		Triangle t = new Triangle();
 		System.out.println(t.area(2, 10));	
-		System.out.println(t.height);
 	}
 	
 }
--- JAVA/Study/src/classEx/Triangle.java
+++ JAVA/Study/src/classEx/Triangle.java
@@ -1,15 +1,18 @@
 package classEx;
 
 public class Triangle {
-	
 
-	int height;
-	int line;
-		
+	// 변수 : 밑변과 높이 
+
+	int height; // 높이 데이터
+	int width; // 밑변 데이터
+	
+	
+	//밑변과 높이 데이터를 변경하는 메서드
 	int area(int a,int b) {
 		height = a;
-		line = b;
-		int result = a * b * 1/2;
+		width = b;
+		int result = height * width * 1/2;
 		return result;
 		
 	}
