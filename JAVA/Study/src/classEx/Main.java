10a56768d8862ee97605a19f4019d0446031105c
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-17 10:06:08 +0900

    삼각형 넓이 클래스

---
+++ JAVA/Study/src/classEx/Main.java
@@ -0,0 +1,11 @@
+package classEx;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Triangle t = new Triangle();
+		System.out.println(t.area(2, 10));	
+		System.out.println(t.height);
+	}
+	
+}
---
+++ JAVA/Study/src/classEx/Triangle.java
@@ -0,0 +1,17 @@
+package classEx;
+
+public class Triangle {
+	
+
+	int height;
+	int line;
+		
+	int area(int a,int b) {
+		height = a;
+		line = b;
+		int result = a * b * 1/2;
+		return result;
+		
+	}
+	
+}
