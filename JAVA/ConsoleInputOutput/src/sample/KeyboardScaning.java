d66433e917b081ee3dddc1bcf5dc04bdb7bde47a
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-20 16:21:05 +0900

    입력

--- JAVA/ConsoleInputOutput/src/sample/KeyboardScaning.java
+++ JAVA/ConsoleInputOutput/src/sample/KeyboardScaning.java
@@ -16,10 +16,9 @@ public class KeyboardScaning {
 		System.out.println("숫자를 입력해주세요");
 		int num3 = sc.nextInt();
 		
-		
 		int sum = 0;
 		sum = num1+num2+num3;
-		System.out.printf("입력된 정수 %d, %d, %d입니다.",num1,num2,num3);
+		System.out.printf("입력된 정수 %d, %d, %d입니다.",num1,num2,num3); 
 		System.out.println("입력한 수의 합 : "+sum);
 		
 	}
