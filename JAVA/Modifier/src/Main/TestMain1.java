54745aeefea25e7437c7cb773313de8a0988a4c7
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-22 12:02:52 +0900

    get set 예제

--- JAVA/Modifier/src/Main/TestMain1.java
+++ JAVA/Modifier/src/Main/TestMain1.java
@@ -7,7 +7,7 @@ public class TestMain1 {
 	public static void main(String[] args) {
 		
 		Member member = new Member(); 
-		System.out.println(member.getId()); //접근 제어지자가 default 값
+		System.out.println(member.getId()); //접근 제어자가 default 값
 		System.out.println(member.getName());
 	}
 
