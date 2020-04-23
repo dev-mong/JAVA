ddf72af2441bf61b1b59f624be4d4023208b0e81
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-23 14:49:07 +0900

    상속 생성자 호출

--- JAVA/Inheritance/src/ex/BusinessMan.java
+++ JAVA/Inheritance/src/ex/BusinessMan.java
@@ -4,10 +4,9 @@ public class BusinessMan extends Man { //하위 클래스
 	
 	String company;
 	String position;
-	
-	
+
 	BusinessMan(String name,String company,String position) {
-		super(name); // 상위 클래스의 생성자 호출 후 name 값 초기화  
+		super("손흥민"); // 상위 클래스의 생성자 호출 후 name 값 초기화  
 		this.company=company;
 		this.position=position;
 	}
--- JAVA/Inheritance/src/ex/BusinessManMain.java
+++ JAVA/Inheritance/src/ex/BusinessManMain.java
@@ -6,7 +6,9 @@ public class BusinessManMain {
 	
 	public static void main(String[] args) {
 		
-		BusinessMan bMan = new BusinessMan("손흥민", "닭집", "대표"); // 인스턴스 생성 
+//		BusinessMan bMan = new BusinessMan("손흥민", "닭집", "대표"); // 값이 모두 존재 할 때
+		//name이 null값일 때 
+		BusinessMan bMan = new BusinessMan(null, "닭집", "대표"); // 인스턴스 생성 
 		
 		bMan.tellYourInto();
 
