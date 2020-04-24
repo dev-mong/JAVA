ac2597b96a6b9d9b346e0a36a599efc6bb15e7a6
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-24 18:33:25 +0900

    친구정보 예제

---
+++ JAVA/Inheritance/src/friend/HeighFriend.java
@@ -0,0 +1,21 @@
+package friend;
+
+public class HeighFriend extends Friend{ //고등학교 친구 클래스
+
+	String work; // 직업 
+	
+	//생성자, 직업 초기화
+	public HeighFriend(String name, String phoneNum, String addr,String work) {
+		super(name, phoneNum, addr);
+		this.work=work;
+	}
+
+
+	@Override
+	void showData() {
+		showBasicInfo();
+		System.out.println("직업: "+this.work);
+	} 
+	
+	
+}
