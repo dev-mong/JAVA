4e04998731e08e5202e8543d9c68e59d3cfc0f20
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-27 14:41:14 +0900

    PhoneBook 0.4 풀이

---
+++ JAVA/PhoneBook/src/ver04/Menu.java
@@ -0,0 +1,17 @@
+package ver04;
+
+public class Menu {
+	
+	//메뉴 메소드
+	public static void showMenu() {
+		System.out.println("==========================");
+		System.out.println("메뉴를 입력하세요");
+		System.out.println("1.정보 저장");
+		System.out.println("2.정보 검색");
+		System.out.println("3.정보 삭제");
+		System.out.println("4.정보 수정");
+		System.out.println("5.정보 전체보기");
+		System.out.println("6.프로그램 종료");
+		System.out.println("==========================");
+	}
+}
---
+++ JAVA/PhoneBook/src/ver04/PhoneInfo.java
@@ -0,0 +1,33 @@
+package ver04;
+
+//상속을 위한 기본 클래스 구성
+//인스턴스 생성의 목적이 없다 
+public class PhoneInfo {
+
+	String name; 		//이름
+	String phoneNumber; //전화번호
+	String addr;        //주소
+	String email;		//이메일
+	
+	//인스턴스 변수들 초기화
+	PhoneInfo(String name,String phoneNumber,String addr,String email){
+		this.name=name;
+		this.phoneNumber=phoneNumber;
+		this.addr=addr;
+		this.email=email;
+	}
+	
+	//기본 정보 출력 메서드
+	void showBasicInfo() {
+		System.out.println("이름: "+this.name);
+		System.out.println("전화번호: "+this.phoneNumber);
+		System.out.println("주소: "+this.addr);
+		System.out.println("이메일: "+this.email);
+	}
+	//전체 정보 출력 메서드
+	void showAllInfo() {
+		// 상속 후 오버라이딩을 통해 재구성 
+		showBasicInfo();
+	}
+	
+}
