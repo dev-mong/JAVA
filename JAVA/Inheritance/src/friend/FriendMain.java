88dfb8e0c7cad225da00787aa2b2c06e55b7374e
a4-01 <qkrwlsaud0118@gmail.com>
2020-04-28 16:47:38 +0900

    인터페이스 예제
    수정 : try-catch문 추가

--- JAVA/Inheritance/src/friend/FriendMain.java
+++ JAVA/Inheritance/src/friend/FriendMain.java
@@ -1,5 +1,10 @@
 package friend;
 
+import java.util.InputMismatchException;
+
+//추가 
+//try-catch 추가
+
 public class FriendMain {
 
 	public static void main(String[] args) {
@@ -17,9 +22,25 @@ public class FriendMain {
 			System.out.println(" "+Menu.EXIT+" 프로그램 종료");
 			System.out.println("=====전체정보 출력=========");
 		
-			int choice = handler.kb.nextInt();
-			handler.kb.nextLine(); // 버퍼 공간 클리어 
-		
+			int choice=0;
+			
+			try {
+				choice = handler.kb.nextInt();
+				//InputMismatchException 오류 발생 하여 catch로 이동 
+			}
+//			catch(Exception e) 
+			catch(InputMismatchException e)
+			{ //InputMismatchException 인스턴스 전달
+				System.out.println("정상적인 메뉴의 번호 입력이 되지 않았습니다.");
+				System.out.println("메뉴를 다시 입력해주세요.");
+				handler.kb.nextLine(); // 버퍼 공간 클리어 
+				continue; // 반복문 마지막 블록으로 이동 => 조건식으로 이동 
+			} catch (Exception e) {
+				System.out.println("예외발생");
+				handler.kb.nextLine(); // 버퍼 공간 클리어 
+				continue; // 반복문 마지막 블록으로 이동 => 조건식으로 이동
+			}
+			
 			switch(choice) {
 			case Menu.INSERT_HEIGH: case Menu.INSERT_UNIV:
 				handler.addFriend(choice);
